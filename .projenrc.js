111f804b58d59ffd370ea2cd2ca70e05a898cba6
Pahud <pahudnet@gmail.com>
2020-10-05 15:35:45 +0800

    update projenrc

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,15 @@
 const {
-  ConstructLibraryAws,
+  AwsCdkConstructLibrary,
+  GithubWorkflow,
 } = require('projen');
 
 const AWS_CDK_LATEST_RELEASE = '1.62.0';
 const PROJECT_NAME = 'cdk-fargate-fastautoscaler';
 const PROJECT_DESCRIPTION = 'A JSII construct lib to build AWS Fargate Fast Autoscaler';
 const PROJECT_REPOSITORY = 'https://github.com/aws-samples/aws-fargate-fast-autoscaler.git';
+const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 
-const project = new ConstructLibraryAws({
+const project = new AwsCdkConstructLibrary({
   "authorName": "Pahud Hsieh",
   "authorEmail": "pahudnet@gmail.com",
   "name": PROJECT_NAME,
@@ -26,7 +28,7 @@ const project = new ConstructLibraryAws({
   },
 
   // creates PRs for projen upgrades
-  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+  // projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
 
   cdkVersion: AWS_CDK_LATEST_RELEASE,
   cdkDependencies: [
@@ -45,6 +47,49 @@ const project = new ConstructLibraryAws({
     distName: 'cdk-fargate-fastautoscaler',
     module: 'cdk_fargate_fastautoscaler'
   }
+});
+
+// create a custom projen and yarn upgrade workflow
+const workflow = new GithubWorkflow(project, 'ProjenYarnUpgrade');
+
+workflow.on({
+  schedule: [{
+    cron: '0 6 * * *'
+  }], // 6am every day
+  workflow_dispatch: {}, // allow manual triggering
+});
+
+workflow.addJobs({
+  upgrade: {
+    'runs-on': 'ubuntu-latest',
+    'steps': [
+      ...project.workflowBootstrapSteps,
+
+      // yarn upgrade
+      {
+        run: `yarn upgrade`
+      },
+
+      // upgrade projen
+      {
+        run: `yarn projen:upgrade`
+      },
+
+      // submit a PR
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v3',
+        with: {
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + '}}',
+          'commit-message': 'chore: upgrade projen',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade projen and yarn',
+          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
+          'labels': 'auto-merge',
+        }
+      },
+    ],
+  },
 });
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'docker-compose.yml', 'images', 'yarn-error.log'];
